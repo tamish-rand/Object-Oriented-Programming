07d80b32ba2b33ae146c98a80d419359cdb5047f
tamish-rand <tamishrt@gmail.com>
2026-09-15 21:38:22 -0700

    Add Hello.cpp and VS Code build task
    Add a simple C++ program (Hello.cpp) that prints the value of x and "Hello, World!", plus a .vscode/tasks.json configured to build the active file with cl.exe. Generated build artifacts (Hello.exe, Hello.obj, Hello.pdb, Hello.ilk, vc140.pdb) are also present in the changeset.

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+   int x = 10;
+   cout << "The value of x is: " << x << endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+
+}
